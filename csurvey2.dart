c9f81f521b1858a9ba0ba7dc23ff046920ee4de9
Niket Dugar <143904117+Niketjr@users.noreply.github.com>
2024-07-12 23:40:04 +0530

    Update csurvey2.dart

--- csurvey2.dart
+++ csurvey2.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'package:flutter/services.dart';
+import 'package:gem/home.dart';
 
-class CSurvey2 extends StatefulWidget {
+class CSurvey2 extends StatelessWidget {
   const CSurvey2({super.key});
 
   @override
-  _CSurvey2State createState() => _CSurvey2State();
-}
-
-class _CSurvey2State extends State<CSurvey2> {
-
-  @override
   Widget build(BuildContext context) {
+    final pincodeController = TextEditingController();
+    final areaController = TextEditingController();
+
     return Scaffold(
       body: SingleChildScrollView(
         child: SizedBox(
@@ -67,147 +65,164 @@ class _CSurvey2State extends State<CSurvey2> {
               ),
               Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      width: 391,
-                      height: 51,
-                      child: Text.rich(
-                        TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'Q  ',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 28,
-                                fontFamily: 'Sawarabi Mincho',
-                                fontWeight: FontWeight.w400,
-                                height: 0,
+                child: Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        width: 391,
+                        height: 51,
+                        child: Text.rich(
+                          TextSpan(
+                            children: [
+                              TextSpan(
+                                text: 'Q  ',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 28,
+                                  fontFamily: 'Sawarabi Mincho',
+                                  fontWeight: FontWeight.w400,
+                                  height: 0,
+                                ),
                               ),
-                            ),
-                            TextSpan(
-                              text: 'Enter your area pincode',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 22,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w400,
-                                height: 0,
+                              TextSpan(
+                                text: 'Enter your area pincode',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 22,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.w400,
+                                  height: 0,
+                                ),
                               ),
-                            ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Container(
+                        width: 287,
+                        height: 56,
+                        decoration: const ShapeDecoration(
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 1.50),
+                          ),
+                        ),
+                        child: TextFormField(
+                          controller: pincodeController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your area pincode';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      const SizedBox(
+                        width: 391,
+                        height: 72,
+                        child: Text.rich(
+                          TextSpan(
+                            children: [
+                              TextSpan(
+                                text: 'Q  ',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 28,
+                                  fontFamily: 'Sawarabi Mincho',
+                                  fontWeight: FontWeight.w400,
+                                  height: 0,
+                                ),
+                              ),
+                              TextSpan(
+                                text: 'Enter your Area (eg. jayanagar)',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 22,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.w400,
+                                  height: 0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Container(
+                        width: 287,
+                        height: 56,
+                        decoration: const ShapeDecoration(
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 1.50),
+                          ),
+                        ),
+                        child: TextFormField(
+                          controller: areaController,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10),
+                          ),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.allow(
+                              RegExp(r'[a-zA-Z\s,\-]'),
+                            ), // Allow letters, spaces, commas, and hyphens
                           ],
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your area';
+                            }
+                            return null;
+                          },
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    Container(
-                      width: 287,
-                      height: 56,
-                      decoration: const ShapeDecoration(
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(width: 1.50),
-                        ),
-                      ),
-                      child: TextFormField(
-                        keyboardType: TextInputType.number,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 30),
-                    const SizedBox(
-                      width: 391,
-                      height: 72, // Increased height to accommodate the text
-                      child: Text.rich(
-                        TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'Q  ',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 28,
-                                fontFamily: 'Sawarabi Mincho',
-                                fontWeight: FontWeight.w400,
-                                height: 0,
-                              ),
-                            ),
-                            TextSpan(
-                              text: 'Enter your Area (eg. jayanagar)',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 22,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w400,
-                                height: 0,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Container(
-                      width: 287,
-                      height: 56,
-                      decoration: const ShapeDecoration(
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(width: 1.50),
-                        ),
-                      ),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 40),
-                    
-                    
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: SizedBox(
-                    width: 390,
-                    height: 60,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  const Home()), // Ensure RSurvey2 is the correct widget name for rsurvey2.dart
-                        );
-                      },
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.resolveWith((states) {
-                          return null; // No specific color gradient applied
-                        }),
-                      ),
-                      child: const Text(
-                        'SUBMIT',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 22,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w400,
-                          height: 0,
-                        ),
-                      ),
-                    ),
+                      const SizedBox(height: 40),
+                    ],
                   ),
                 ),
               ),
             ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const Home()),
+          );
+        },
+        child: Container(
+          height: 80,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(1, 0),
+              end: Alignment(0, 1),
+              colors: [
+                Color.fromRGBO(255, 255, 255, 0.5),
+                Color.fromRGBO(34, 124, 112, 1)
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'BACK',
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 36,
+                color: Colors.black,
+              ),
+            ),
           ),
         ),
       ),
